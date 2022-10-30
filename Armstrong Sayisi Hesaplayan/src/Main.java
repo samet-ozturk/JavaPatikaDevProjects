e1b5e2d77b2a8cc0bfcbd6295b96e937d4a27a57
samet öztürk <95880268+samet-ozturk@users.noreply.github.com>
2022-10-30 23:55:55 +0300

    Add files via upload

---
+++ Armstrong Sayisi Hesaplayan/src/Main.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Bir Değer Giriniz: ");
+        int basamak,basamakTop=0;
+
+        Scanner scan= new Scanner(System.in);
+
+        System.out.println("Lütfen bir sayı giriniz: ");
+        int num = scan.nextInt();
+
+        while (num!=0){
+
+            basamak=num%10;
+            basamakTop+=basamak;
+            num/=10;}
+        System.out.println(basamakTop);
+
+    }
+}
